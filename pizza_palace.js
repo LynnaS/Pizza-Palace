cfdf819a7921399951ca7c9d690b93e7778dbeb7
Lynna Schaldemose <lynnaschaldemose@gmail.com>
2017-03-01 17:14:53 -0800

    added two functions
    one for each price

--- pizza_palace.js
+++ pizza_palace.js
@@ -1,1 +1,11 @@
-<!--getting ready for javascript-->
+var count=0;
+function myFunction() {
+  count++ ;
+  document.getElementById('price1').innerHTML = "Your total will be " = count;
+  }
+
+var count=0;
+function myFunction() {
+  count++ ;
+  document.getElementById('price3').innerHTML = "Your total will be " = count;
+  }
